f11a9a2aaa0e266c289d3fa1f1056e78cf2ad4a2
Ritika5657 <ritikapandey5657@gmail.com>
2025-08-26 13:22:25 +0530

    Initial4

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@ import joblib
 from sklearn.ensemble import RandomForestClassifier
 
 class TestModelTraining(unittest.TestCase):
-	def test_model_training(self)
+	def test_model_training(self):
 		model = joblib.load('model/iris_model.pkl')
 		self.assertIsInstance(model, RandomForestClassifier)
 		self.assertGreaterEqual(len(model.feature_importances_), 4)
